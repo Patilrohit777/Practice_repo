e2b9b60c14d1cfc78a4fe27518f9d987a048c8ed
Rohit Patil <patilr566@gmail.com>
2024-09-25 00:20:59 +0530

    hii first resource is created

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,11 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+# outputs.tf
+
+output "instance_id" {
+  description = "The ID of the created EC2 instance"
+  value       = aws_instance.my_ec2_instance.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
-
-output "public_instance_id" {
-  value = aws_instance.public_instance.id
-}
-
-output "private_instance_id" {
-  value = aws_instance.private_instance.id
-}
-
-output "nat_gateway_id" {
-  value = aws_nat_gateway.nat.id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.gw.id
+output "instance_public_ip" {
+  description = "The public IP of the created EC2 instance"
+  value       = aws_instance.my_ec2_instance.public_ip
 }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+# terraform.tfvars
+
+ami_id        = "ami-0ebfd941bbafe70c6" # Replace with a valid AMI ID for your region
+instance_type = "t2.micro"               # Optional: you can change this if needed
