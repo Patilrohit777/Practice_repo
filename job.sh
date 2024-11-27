0e8c658b7ea7c80ad7a6b3489ffaf66f183e6d63
Patilrohit777 <110105295+Patilrohit777@users.noreply.github.com>
2024-11-27 18:07:50 +0530

    Update job.sh echo statement added
    ec

--- job.sh
+++ job.sh
@@ -1,14 +1,10 @@
 
 echo "running terraform init command"
-terraform init
+echo "running terraform init command"
+echo "running terraform init command"
+echo "running terraform init command"
+echo "running terraform init command"
+echo "running terraform init command"
+echo "running terraform init command10000"
 
-echo "terraform init running completed and now running terraform plan command"
-
-terraform plan
-
-echo "terraform plan command running is completed"
-
-echo "running terraform apply command"
-
-terraform apply
 
